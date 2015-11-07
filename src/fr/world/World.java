481860675cb9b77a03f7530d99017a99296114b0
Seiwert Quentin <quentin.seiwert@telecomnancy.net>
2015-11-07 02:14:59 +0100

    World map

--- src/fr/world/World.java
+++ src/fr/world/World.java
@@ -19,6 +19,7 @@ public class World extends BasicGameState{
 	private static ArrayList<Enemy> enemies;
 	private static Enemy enemyTest;
 	private static ArrayList<Weapon> projectiles;
+	private Decor decor;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
@@ -26,11 +27,14 @@ public class World extends BasicGameState{
 		enemies = new ArrayList<Enemy>();
 		enemyTest = new Enemy();
 		projectiles = new ArrayList<Weapon>();
+		decor = new Decor("assets/box.png","assets/background.png");
+		decor.init(container,game);
 		
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		decor.render(container,game,g);
 		player.render(container, game, g);
 		for(int i = 0; i<enemies.size();i++){
 			enemies.get(i).render(container, game, g);
@@ -42,6 +46,7 @@ public class World extends BasicGameState{
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		decor.update(container,game,delta);
 		player.update(container, game, delta);
 		for(int i = 0; i<enemies.size();i++){
 			enemies.get(i).update(container, game, delta);
@@ -53,11 +58,13 @@ public class World extends BasicGameState{
 	
 	public void keyReleased(int key, char c) {
 		player.keyReleased(key, c);
+		decor.keyRealeased(key,c);
 	}
 
 
 	public void keyPressed(int key, char c) {
 		player.keyPressed(key, c);
+		decor.keyPressed(key,c);
 	}
 
 	@Override
